995b6ce0e2f5ec878e0cb078781ae4136ae03618
Max Spahn <max.spahn@de.abb.com>
2025-01-27 14:42:43 +0100

    Adds windows configuration of nvim.

---
+++ dotfiles/config/nvim-win/lua/config/keymaps.lua
@@ -0,0 +1,39 @@
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+-- Save
+map("n", "<leader>w", "<CMD>update<CR>")
+
+-- Quit
+map("n", "<leader>q", "<CMD>q<CR>")
+map("n", "<leader>x", "<CMD>q!<CR>")
+map("i", "<leader>x", "<ESC><CMD>q!<CR>")
+-- Close buffer
+map("n", "<leader>d", "<CMD>bd<CR>")
+map("n", "<Leader>n", ":bn <CR>")
+map("n", "<Leader>p", ":bp <CR>")
+
+-- Exit insert mode
+map("i", "<leader>,", "<ESC>")
+map("v", "<leader>,", "<ESC>")
+
+-- New Windows
+map("n", "<Leader>v", ":Vex <CR>")
+map("n", "<Leader>s", ":Sex <CR>")
+
+-- Window Navigation
+map("n", "<leader>h", "<C-w>h")
+map("n", "<leader>l", "<C-w>l")
+map("n", "<leader>k", "<C-w>k")
+map("n", "<leader>j", "<C-w>j")
+
+-- Resize Windows
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
+
+-- Config
+map("n", "<leader>sv", "<CMD>source $MYVIMRC<CR>")
+
---
+++ dotfiles/config/nvim-win/lua/config/options.lua
@@ -0,0 +1,42 @@
+local global = vim.g
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.syntax = "on"
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.encoding = "UTF-8"
+o.ruler = true
+o.mouse = "a"
+o.title = true
+o.hidden = true
+o.ttimeoutlen = 0
+o.wildmenu = true
+o.showcmd = true
+o.showmatch = true
+o.inccommand = "split"
+o.splitright = true
+o.splitbelow = true
+o.termguicolors = true
+o.completeopt = 'menuone,noselect'
+o.colorcolumn = '120'
+o.textwidth = 120
+o.background = "dark"
+o.autochdir = true
+o.fileformat = "unix"
+
+vim.cmd("colorscheme gruvbox")
+global.loaded_python3_provider = 1
+local user_profile = vim.fn.getenv 'USERPROFILE'
+vim.g.python3_host_prog = user_profile .. '/.pyenv/pyenv-win/versions/3.10.11/python.exe'
+-- global.python3_host_prog = '"C:/Program Files/WindowsApps/PythonSoftwareFoundation.Python.3.11_3.11.2544.0_x64__qbz5n2kfra8p0/python3.11.exe"'
+-- global.python3_host_prog = 'C:/Users/DEMASPA1/AppData/Local/Microsoft/WindowsApps/python3.exe'
+
+vim.cmd [[let &shell = '"C:/Users/DEMASPA1/AppData/Local/Programs/Git//bin/bash.exe"']]
+vim.cmd [[let &shellcmdflag = '-s']]
+
