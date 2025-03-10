c4312fb57150026aff88e810238f183e0a5444a9
Max Spahn <m.spahn@tudelft.nl>
2025-03-10 09:49:21 +0100

    ft[ros]: Corrects ros workspace indication paranthesis.

--- dotfiles/config/shell/ps1.zsh
+++ dotfiles/config/shell/ps1.zsh
@@ -1,6 +1,6 @@
 
 
 setopt PROMPT_SUBST
-export PROMPT='($(get_ros_ws))%F{blue}[$(whoami)@$ROBOT_NAME]%F{green}[-%2d]%f '
+export PROMPT='$(get_ros_ws)%F{blue}[$(whoami)@$ROBOT_NAME]%F{green}[-%2d]%f '
 export RPROMPT='%F{$(git_is_up_to_date)}$(git_prompt)'
 
