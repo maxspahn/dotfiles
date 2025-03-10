2a1ec4e2a2e0c246985f7dad0113667708e4d431
Max Spahn <m.spahn@tudelft.nl>
2025-03-10 09:46:25 +0100

    Adds ros worspace indication in prompt.

--- dotfiles/config/shell/ps1.zsh
+++ dotfiles/config/shell/ps1.zsh
@@ -1,22 +1,6 @@
-git_prompt() {
-    local branch="$(git symbolic-ref HEAD 2> /dev/null | cut -d'/' -f3-)"
-    local branch_truncated="${branch:0:30}"
 
-    if (( ${#branch} > ${#branch_truncated} )); then
-        branch="${branch_truncated}..."
-    fi
-
-    [ -n "${branch}" ] && echo " (${branch})"
-}
-git_is_up_to_date() {
-    local changes="$(git status --porcelain 2> /dev/null)"
-    local color="green"
-    if [[ ${#changes} -gt 5 ]] ; then
-      local color="red"
-    fi
-    echo ${color}
-}
 
 setopt PROMPT_SUBST
-export PROMPT='%F{blue}[$(whoami)@$ROBOT_NAME]%F{green}[-%2d]%f '
+export PROMPT='($(get_ros_ws))%F{blue}[$(whoami)@$ROBOT_NAME]%F{green}[-%2d]%f '
 export RPROMPT='%F{$(git_is_up_to_date)}$(git_prompt)'
+
